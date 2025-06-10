7425d1c8807146b271918072db5d6199b1f78e06
千山暮雪 <li23258@qq.com>
2025-06-10 16:22:12 +0800

    initialize(repo): First commit.

---
+++ src/main/java/com/laboratory_sys/demo/LaboratoryBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.laboratory_sys.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryBookingSystemApplication.class, args);
+	}
+
+}
