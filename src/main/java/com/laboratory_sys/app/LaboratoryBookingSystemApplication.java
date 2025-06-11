7be866d65b657dd836b7ceee048f932ff194d009
千山暮雪 <li23258@qq.com>
2025-06-11 10:35:31 +0800

    feature: add Greeting module for test.

--- src/main/java/com/laboratory_sys/app/LaboratoryBookingSystemApplication.java
+++ src/main/java/com/laboratory_sys/app/LaboratoryBookingSystemApplication.java
@@ -3,11 +3,14 @@ package com.laboratory_sys.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// import com.laboratory_sys.app.GreetingController;
+
 @SpringBootApplication
 public class LaboratoryBookingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaboratoryBookingSystemApplication.class, args);
+		// GreetingController greetingController = new GreetingController();
 		System.out.println("Laboratory Booking System Application has started successfully.");
 	}
 
